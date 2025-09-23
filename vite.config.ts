5a34efce99f7fc6434f54caabfd023563af43fa7
Veena Madhu <vmadhu@tudelft.nl>
2025-09-23 19:00:03 +0200

    changes vite.config to correct base

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/veenamadhu-website/', // <- important for GitHub Pages
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
